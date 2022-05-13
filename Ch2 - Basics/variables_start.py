4c896d329e828abb799169af0146a77fa3811079
abdghaniallag <abdghaniallag@gmail.com>
2022-05-13 04:42:22 +0100

    terminate variables_start.py lesson

--- Ch2 - Basics/variables_start.py
+++ Ch2 - Basics/variables_start.py
@@ -22,16 +22,25 @@ print(mytuple)
 print(mydict)
 
 # re-declaring a variable works
-
+# myint = "abc"
+print(myint)
 # to access a member of a sequence type, use []
-
+print(mylist[2])
 # use slices to get parts of a sequence
-
+print(mylist[2:3])
 # you can use slices to reverse a sequence
-
+print(mytuple[::-1])
 # dictionaries are accessed via keys
-
+print(mydict["two"])
 # ERROR: variables of different types cannot be combined
-
+print("arsdf " +  str(542))
 # Global vs. local variables in functions
 
+def changeStr ():
+   # global mystr
+    mystr = " 500"
+    print(mystr)
+changeStr()
+print(mystr)
+del mystr
+print(mystr)
