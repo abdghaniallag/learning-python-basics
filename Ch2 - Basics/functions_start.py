6c0be8e0b08fdf96bd54dc038237e16918bb004e
abdghaniallag <abdghaniallag@gmail.com>
2022-05-13 05:09:03 +0100

    terminate functions_start.py lesson

--- Ch2 - Basics/functions_start.py
+++ Ch2 - Basics/functions_start.py
@@ -5,17 +5,33 @@
 
 
 # TODO: define a basic function
-
+def functionOne():
+    print("functionOne")
 
 # TODO: function that takes arguments
+def functionTwo(argumentOne ,argumentTwo ):
+    print(argumentOne+argumentOne)
 
 
 # TODO: function that returns a value
+def cube(x=1):
+    return x*x*x
 
 
 # TODO: function with default value for an argument
-
+def power(number , x=1):
+    result=1;
+    for i in range(x):
+        result=result*number
+    return result
 
 # TODO: function with variable number of arguments
+def multiAdd(*args):
+    result =0
+    for i in args:
+        result = result + i
+    return result
 
-
+functionOne()
+print(functionTwo("arfsv","arsgfwedsf "))
+print(multiAdd(4,8,8,8,7))
