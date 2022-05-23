e2b1d04c53c886cb9b0174fb8f3f391502b6b21e
abdghaniallag <abdghaniallag@gmail.com>
2022-05-23 03:40:16 +0100

    terminate loop lesson

--- Ch2 - Basics/loops_start.py
+++ Ch2 - Basics/loops_start.py
@@ -8,20 +8,39 @@ def main():
     x = 0
 
     # TODO: define a while loop
-
-
+    """
+    while (x < 7) :
+        print(x)
+        x = x+1  
+    """
     # TODO: define a for loop
-
+    """
+        for x in range(5,10):
+        print(x)
+    """
 
     # TODO: use a for loop over a collection
+    """
     days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
-
-
+    for d in days:
+        print(d)
+    """
     # TODO: use the break and continue statements
+    """
+        days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    for d in days:
+       if(d=="Sat"):
+            break
+       if(d=="Tue"):
+            continue
+       print(d)
 
-
+    """
     # TODO: using the enumerate() function to get index 
-    
+    days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    for i,d in enumerate( days):
+       print(i,d)
+
   
 if __name__ == "__main__":
     main()
