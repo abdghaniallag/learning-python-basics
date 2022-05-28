1f1d90b639c57daabfdfef0b49644c8286b640c4
abdghaniallag <abdghaniallag@gmail.com>
2022-05-28 03:48:23 +0100

    terminate Modules lesson

--- Ch2 - Basics/modules_start.py
+++ Ch2 - Basics/modules_start.py
@@ -3,12 +3,15 @@
 
 
 # TODO: import the math module, which contains features for working with mathematics
-
+import math
 
 # TODO: the math module contains lots of pre-built functions
 
+print("the square root of 81 is", math.sqrt(81))
+# TODO: in addition to functions, some modules contain useful constants
 
-# TODO: in addition to functions, some modules contain useful constants 
+print("Pi is", math.pi)
 
 
 # TODO: try some of the math functions for yourself here:
+print("the distance between point A(99,0) and B(5,6) is", math.dist((99,0),(5,6)))
