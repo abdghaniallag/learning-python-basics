3d5f6e2933106c33b2a0c7016ca77d4debf7c651
abdghaniallag <abdghaniallag@gmail.com>
2022-07-06 07:44:48 +0100

    terminate chapter 5

--- Ch5 - Internet Data/inetdata_start.py
+++ Ch5 - Internet Data/inetdata_start.py
@@ -2,9 +2,19 @@
 # Example file for retrieving data from the internet
 # LinkedIn Learning Python course by Joe Marini
 #
+import urllib.request
+
 
 def main():
-    pass # this is a placeholder, do-nothing statement
+    webUrl = urllib.request.urlopen("http://www.google.com/")
+
+    # get the result code and print it
+    print("result code: ", webUrl.getcode())
+
+    # read the data from the URL and print it
+    data = webUrl.read()
+    print(data)
+
 
 if __name__ == "__main__":
     main()
